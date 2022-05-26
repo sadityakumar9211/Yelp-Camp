b47f4a66032c6fbf5650dd37a23b94902607d870
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-05-26 23:25:41 +0530

    ready to try deploying

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const mongoSanitize = require('express-mongo-sanitize')
 const MongoStore = require('connect-mongo')(session)
 const helmet = require('helmet')
 
-const dbUrl = process.env.DB_URL
-// const dbUrl = 'mongodb://localhost:27017/yelp-camp'
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 //Connecting the Database
 async function main() {
     try {
@@ -44,9 +44,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, '/public')))
 
+const secret = process.env.SECRET||'32fajdlf'
 const store = new MongoStore({
     url: dbUrl,
-    secret:'32fajdlf',
+    secret:secret,
     touchAfter: 24*60*60
 })
 
@@ -57,7 +58,7 @@ store.on('error',function(e){
 const sessionConfig = {
     store,
     name: 'session',
-    secret: '32fajdlf',
+    secret: secret, 
     resave: false,
     saveUninitialized: true,
     //Adding fancier options for the cookie itself.
